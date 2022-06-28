d134407ec9506d84ddaf088bffb8fe170c12d959
clover-leaf <thangnguyenmatt@gmail.com>
2022-06-29 00:57:20 +0700

    edit notice page: add padding

--- lib/app_notification/view/app_notification_page.dart
+++ lib/app_notification/view/app_notification_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_firestore/app_notification/app_notification.dart';
+import 'package:flutter_firestore/common/common.dart';
 
 class AppNotificationPage extends StatelessWidget {
   const AppNotificationPage({super.key});
@@ -19,6 +20,17 @@ class AppNotificationView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('AppNotification');
+    return Padding(
+      padding: EdgeInsets.fromLTRB(
+        Space.contentPaddingLeft.value,
+        Space.contentPaddingTop.value + MediaQuery.of(context).viewPadding.top,
+        Space.contentPaddingRight.value,
+        Space.contentPaddingBottom.value,
+      ),
+      child: const Text(
+        'Notices',
+        style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+      ),
+    );
   }
 }
