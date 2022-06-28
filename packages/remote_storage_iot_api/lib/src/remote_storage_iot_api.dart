cf1194fb4507b4ca35a8e8cd28d2d88f64bd88f7
clover-leaf <thangnguyenmatt@gmail.com>
2022-06-29 00:58:00 +0700

    update new host IP

--- packages/remote_storage_iot_api/lib/src/remote_storage_iot_api.dart
+++ packages/remote_storage_iot_api/lib/src/remote_storage_iot_api.dart
@@ -17,7 +17,7 @@ class RemoteStorageIotApi extends IotApi {
   /// iOS emulator: 127.0.0.1 or localhost
   /// Android emulator: 10.0.2.2
   /// real device: 192.168.1.3 (host ip)
-  static const String kBaseURL = '192.168.1.5:9876';
+  static const String kBaseURL = '192.168.1.4:9876';
 
   /// schema name
   static const String schema = 'thang';
