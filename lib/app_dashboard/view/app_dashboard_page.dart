9d6890539b9f997f3b1030d52be88ab5c8e30102
clover-leaf <thangnguyenmatt@gmail.com>
2022-06-29 00:55:39 +0700

    add dashboard page with 10 line chart example

--- lib/app_dashboard/view/app_dashboard_page.dart
+++ lib/app_dashboard/view/app_dashboard_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_firestore/app_dashboard/app_dashboard.dart';
+import 'package:flutter_firestore/common/common.dart';
 
 class AppDashboardPage extends StatelessWidget {
   const AppDashboardPage({super.key});
@@ -19,6 +20,71 @@ class AppDashboardView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('AppDashboard');
+    return Padding(
+      padding: EdgeInsets.fromLTRB(
+        Space.contentPaddingLeft.value,
+        Space.contentPaddingTop.value + MediaQuery.of(context).viewPadding.top,
+        Space.contentPaddingRight.value,
+        Space.contentPaddingBottom.value,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Dashboard',
+            style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 8),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8),
+                    child: LineChartSample1(),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8),
+                    child: LineChartSample2(),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8),
+                    child: LineChartSample3(),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8),
+                    child: LineChartSample4(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: LineChartSample6(),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8),
+                    child: LineChartSample5(),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8),
+                    child: LineChartSample7(),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8),
+                    child: LineChartSample8(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: LineChartSample9(),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8),
+                    child: LineChartSample10(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/app_dashboard/widgets/widgets.dart
@@ -0,0 +1,10 @@
+export 'line_chart_1.dart';
+export 'line_chart_10.dart';
+export 'line_chart_2.dart';
+export 'line_chart_3.dart';
+export 'line_chart_4.dart';
+export 'line_chart_5.dart';
+export 'line_chart_6.dart';
+export 'line_chart_7.dart';
+export 'line_chart_8.dart';
+export 'line_chart_9.dart';
