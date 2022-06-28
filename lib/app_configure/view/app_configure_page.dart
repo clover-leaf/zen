a54b34beaecb2736b41b84945d8a699e66198daf
clover-leaf <thangnguyenmatt@gmail.com>
2022-06-29 00:58:19 +0700

    edit configure page: add padding

--- lib/app_configure/view/app_configure_page.dart
+++ lib/app_configure/view/app_configure_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_firestore/app_configure/app_configure.dart';
+import 'package:flutter_firestore/common/common.dart';
 
 class AppConfigurePage extends StatelessWidget {
   const AppConfigurePage({super.key});
@@ -19,6 +20,17 @@ class AppConfigureView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('AppConfigure');
+    return Padding(
+      padding: EdgeInsets.fromLTRB(
+        Space.contentPaddingLeft.value,
+        Space.contentPaddingTop.value + MediaQuery.of(context).viewPadding.top,
+        Space.contentPaddingRight.value,
+        Space.contentPaddingBottom.value,
+      ),
+      child: const Text(
+        'Setting',
+        style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+      ),
+    );
   }
 }
